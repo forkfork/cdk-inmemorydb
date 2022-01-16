4ada1a70869d5463f16024c015fde8e32bb60525
Timothy Downs <timothydowns@gmail.com>
2022-01-16 21:08:12 +1100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Timothy Downs',
+  authorAddress: 'timothydowns@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-redisdb',
+  repositoryUrl: 'https://github.com/timothydowns/cdk-redisdb.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
